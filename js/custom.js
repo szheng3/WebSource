46ee82313645f78a87a231b2fd1b65377dab0fe9
Shuai Zheng <szheng3@uci.edu>
2017-05-27 18:35:54 -0700

    change to zz

--- js/custom.js
+++ js/custom.js
@@ -216,26 +216,26 @@ jQuery(document).ready(function () {
 });
 
 
-
-$(document).ready(function() {
-  $('#contact-form').submit(function(e) {
-    var name    = document.getElementById('inputName')
-    var email   = document.getElementById('inputEmail')
-    var message = document.getElementById('inputMessage')
-
-    if (!name.value || !email.value || !message.value) {
-      alertify.error("Please check your entries");
-      return false;
-    } else {
-      $.ajax({
-        method: 'POST',
-        url: '//formspree.io/youremail@uci.edu',
-        data: $('#contact-form').serialize(),
-        datatype: 'json'
-      });
-      e.preventDefault();
-      $(this).get(0).reset();
-      alertify.success("Message sent");
-    }
-  });
-});
+//
+// $(document).ready(function() {
+//   $('#contact-form').submit(function(e) {
+//     var name    = document.getElementById('inputName')
+//     var email   = document.getElementById('inputEmail')
+//     var message = document.getElementById('inputMessage')
+//
+//     if (!name.value || !email.value || !message.value) {
+//       alertify.error("Please check your entries");
+//       return false;
+//     } else {
+//       $.ajax({
+//         method: 'POST',
+//         url: '//formspree.io/youremail@uci.edu',
+//         data: $('#contact-form').serialize(),
+//         datatype: 'json'
+//       });
+//       e.preventDefault();
+//       $(this).get(0).reset();
+//       alertify.success("Message sent");
+//     }
+//   });
+// });
