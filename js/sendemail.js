f4803dc298933690275f9685ed887767f2192e02
Shuai Zheng <szheng3@uci.edu>
2017-06-07 12:20:07 -0700

    added contact us

---
+++ js/sendemail.js
@@ -0,0 +1,42 @@
+/**
+ * Created by zhengshuai on 6/7/17.
+ */
+
+$(document).ready(function () {
+
+    $('#someForm').on('submit', function (e) {
+        e.preventDefault();
+
+        //get the name field value
+        var name = $('#name').val();
+        //get the name field value
+        var email = $('#email').val();
+        //get the comments
+        var comments = $('#message').val();
+        var subject = $('#subject').val();
+
+
+        //pretend we don't need validation
+
+        //send to formspree
+        $.ajax({
+            url: 'https://formspree.io/szheng3@uci.edu',
+            method: 'POST',
+            data: {
+                name: name,
+                _replyto: email,
+                email: email,
+                comments: comments,
+                _subject: subject,
+            },
+            dataType: "json",
+            success: function () {
+                console.log('success');
+                alertify.success("Message sent");
+            }
+
+        });
+
+    });
+
+});
